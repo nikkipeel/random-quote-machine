097fd3bac7e3035c50d08928272afcb75278c388
Nikki <npeel3497@gmail.com>
2020-02-29 11:08:38 -0500

    Add QuoteMachine component and functionality to Button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { random } from "lodash";
 import './App.css';
 import Button from "./components/Button";
+import QuoteMachine from "./components/QuoteMachine";
 
 class App extends Component {
     constructor(props) {
@@ -12,37 +13,40 @@ class App extends Component {
             ],
             selectedQuoteIndex: null,
         }
-        this.selectedQuoteIndex = this.selectedQuoteIndex.bind(this);
+        this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
+        this.generateNewQuoteIndex = this.generateNewQuoteIndex.bind(this);
     }
 
     componentDidMount() {
         fetch("https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json")
             .then(data => data.json())
-            .then(quotes => this.setState({ quotes }, () => {
-                this.setState({ selectedQuoteIndex: this.selectedQuoteIndex()})
-            }));
+            .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
     }
 
     get selectedQuote() {
         if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
-            return;
+            return undefined;
         }
         return this.state.quotes[this.state.selectedQuoteIndex];
     }
 
-    selectedQuoteIndex() {
+    generateNewQuoteIndex() {
         if (!this.state.quotes.length) {
-            return;
+            return undefined;
         }
         return random(0, this.state.quotes.length - 1);
+    }
+
+    assignNewQuoteIndex() {
+        this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() })
+
     }
 
     render() {
         return (
             <div className="App" id="quote-box">
-                {this.selectedQuote ? `"${this.selectedQuote.quote}" - ${this.selectedQuote.author}` : '' }
-                <Button buttonDisplayName="Next Quote" clickHandler={this.nextQuoteClickHandler} />
-            </div>
+                <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} />
+                     </div>
         );
     }
 }
